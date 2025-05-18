a32340394ca9706eaca1e9b6b07bfd724c8eae81
Vii <itsviiuwu@gmail.com>
2025-05-19 00:10:27 +0300

    Initial override commit, mostly undocumented, sorry

--- assets/js/searchdata.js
+++ assets/js/searchdata.js
@@ -11,34 +11,38 @@ var jsondata=[
       "tags"     : "{{ post.tags | join: ', ' }}",
       "url"      : "{{ site.baseurl }}{{ post.url }}",
       "date"     : "{{ post.date }}",
-      "content"  : {{ page.content | jsonify }}
+      "excerpt"  : {{ post.excerpt | strip_html | jsonify }},
+      "content"  : ""
     } {% unless forloop.last %},{% endunless %}
   {% endfor %}
   ,
   {% for page in site.html_pages %}
+  {% if page.is_wiki_page != false %}
    {
      {% assign title = page.title | default: page.name %}
      {% if title != nil %}
+     {% assign excerpt = page.excerpt | default: page.content | markdownify | strip_html | truncatewords: 10 | jsonify %}
         "title"    : "{{ title | escape }}",
         "category" : "{{ page.category }}",
         "tags"     : "{{ page.tags | join: ', ' }}",
         "url"      : "{{ site.baseurl }}{{ page.url }}",
         "date"     : "{{ page.date }}",
-        "content"  : {{ page.content | jsonify }}
-     {% endif %}
-   } {% unless forloop.last %},{% endunless %}
+        "excerpt"  :  {{excerpt}},
+        "content"  :  ""
+      {% endif %}
+    } {% unless forloop.last %},{% endunless %}
+    {% endif %}
   {% endfor %}
 ];
 
 var sjs = SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
-    resultsContainer: document.getElementById('results-container'),
+    resultsContainer: document.getElementById('results'),
     json: jsondata,
-    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
-    noResultsText: 'No results found',
-    limit: 10,
-    fuzzy: false,
+    searchResultTemplate: '<li><a href="{url}" title="{excerpt}">{title}</a></li>',
+    noResultsText: 'Nothing here.',
+    limit: 20,
+    fuzzy: true,
     exclude: []
   })
 {% endif %}
-
